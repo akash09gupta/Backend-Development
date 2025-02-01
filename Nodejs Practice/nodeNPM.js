c39c65796643a08407b1ed7352a969bfe5c38bef
akash09gupta <akashgupta211ag@gmail.com>
2025-02-01 23:19:01 +0530

    NPM packages installing, uninstalliung and practicing how to create own scripts

---
+++ Nodejs Practice/nodeNPM.js
@@ -0,0 +1,6 @@
+//NPM Contains packages
+// for install any packege - npm i one-liner-joke
+// for uninstall any package - npm unistall one-liner-joke
+// for install any package with particular version - npm i accessibility@4.4.1
+// we install some devDependencies which used only in the development time but automatically removing on deployment time - npm i nodemon --save-dev
+
